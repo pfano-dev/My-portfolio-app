02b3f02d93d30f1c7ef73ce68b58a0370e80d154
muleya pfano <79614241+pfano-dev@users.noreply.github.com>
2023-04-21 11:09:45 +0200

    my portfolio

---
+++ components/Sidebar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import profile from "../assets/images/about.jpg"
+
+function Sidebar() {
+  return (
+    <div>
+      <img  src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg" 
+      alt="profile" />
+      <h3>
+        <span>sumit</span>
+        Dey
+      </h3>
+      <p>Software Engineer</p>
+      <p>Download</p>
+    </div>
+  )
+}
+
+export default Sidebar
